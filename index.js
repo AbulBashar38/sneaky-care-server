0d2191f0a197c54443e5d9c753e0022445698e6b
Abul Basar <76747711+AbulBashar38@users.noreply.github.com>
2024-05-12 13:54:02 +0600

    update controller

--- index.js
+++ index.js
@@ -19,164 +19,203 @@ const client = new MongoClient(uri, {
   },
 });
 
-
 async function run() {
   try {
-    client.connect().then(() => {
-      console.log("Connected to MongoDB");
+    client
+      .connect()
+      .then(() => {
+        console.log("Connected to MongoDB");
 
-      const database = client.db(`${process.env.DB_NAME}`);
-      const productCollection = database.collection("allProduct");
-      const serviceCollection = database.collection("services");
-      const orderCollection = database.collection("Orders");
-      const testimonialCollection = database.collection("testimonials");
-      const adminCollection = database.collection("admins");
-      const appointmentCollection = database.collection("appointment");
-      const bookingAppointmentCollection =
-        database.collection("bookingAppointment");
-      // Define routes that depend on MongoDB connection
-      app.use(express.json()); // Middleware to parse JSON bodies
+        const database = client.db(`${process.env.DB_NAME}`);
+        const productCollection = database.collection("allProduct");
+        const serviceCollection = database.collection("services");
+        const orderCollection = database.collection("orders");
+        const testimonialCollection = database.collection("testimonials");
+        const adminCollection = database.collection("admins");
+        const appointmentCollection = database.collection("appointment");
+        const bookingAppointmentCollection =
+          database.collection("bookingAppointment");
+        // Define routes that depend on MongoDB connection
+        app.use(express.json()); // Middleware to parse JSON bodies
 
-      app.post("/addAService", (req, res) => {
-        serviceCollection.insertOne(req.body).then((result) => {
-          res.send(result.acknowledged);
-        });
-      });
-
-      app.post("/addTestimonial", (req, res) => {
-        testimonialCollection.insertOne(req.body).then((result) => {
-          res.send(result.acknowledged);
-        });
-      });
-
-      app.post("/addAAdmin", (req, res) => {
-        adminCollection.insertOne(req.body).then((result) => {
-          res.send(result.acknowledged);
-        });
-      });
-
-      app.post("/addAProduct", (req, res) => {
-        productCollection.insertOne(req.body).then((result) => {
-          res.send(result.acknowledged);
-        });
-      });
-
-      app.post("/addOrder", (req, res) => {
-        orderCollection.insertOne(req.body).then((result) => {
-          console.log(result);
-          res.send(result.acknowledged);
-        });
-      });
-
-      app.post("/addAppointment", (req, res) => {
-        bookingAppointmentCollection.insertOne(req.body).then((result) => {
-          res.send(result.acknowledged);
-        });
-      });
-
-      app.get("/getServices", (req, res) => {
-        serviceCollection.find({}).toArray((err, document) => {
-          res.send(document);
-        });
-      });
-
-      app.get("/appointments", (req, res) => {
-        appointmentCollection.find({}).toArray((err, document) => {
-          res.send(document);
-        });
-      });
-
-      app.get("/checkAdmin", (req, res) => {
-        adminCollection
-          .find({ adminEmail: req.query.email })
-          .toArray((err, document) => {
-            res.send(document);
-          });
-      });
-
-      app.delete("/deleteService/:id", (req, res) => {
-        serviceCollection
-          .deleteOne({ _id: ObjectID(req.params.id) })
-          .then((result) => {
+        app.post("/addAService", (req, res) => {
+          serviceCollection.insertOne(req.body).then((result) => {
             res.send(result.acknowledged);
           });
-      });
+        });
 
-      app.get("/getOrder", (req, res) => {
-        orderCollection
-          .find({ email: req.query.email })
-          .toArray((err, document) => {
-            res.send(document);
+        app.post("/addTestimonial", (req, res) => {
+          testimonialCollection.insertOne(req.body).then((result) => {
+            res.send(result.acknowledged);
           });
-      });
+        });
 
-      app.get("/getAppointment", (req, res) => {
-        bookingAppointmentCollection
-          .find({ email: req.query.email })
-          .toArray((err, document) => {
-            res.send(document);
+        app.post("/addAAdmin", (req, res) => {
+          adminCollection.insertOne(req.body).then((result) => {
+            res.send(result.acknowledged);
           });
-      });
+        });
 
-      app.get("/orderedProduct/:id", (req, res) => {
-        productCollection
-          .find({ _id: ObjectID(req.params.id) })
-          .toArray((err, document) => {
-            if (document.length === 0) {
-              appointmentCollection
-                .find({ _id: ObjectID(req.params.id) })
-                .toArray((err, document) => {
-                  res.send(document[0]);
-                });
+        app.post("/addAProduct", (req, res) => {
+          productCollection.insertOne(req.body).then((result) => {
+            res.send(result.acknowledged);
+          });
+        });
+
+        app.post("/addOrder", (req, res) => {
+          orderCollection.insertOne(req.body).then((result) => {
+            console.log(result);
+            res.send(result.acknowledged);
+          });
+        });
+
+        app.post("/addAppointment", (req, res) => {
+          bookingAppointmentCollection.insertOne(req.body).then((result) => {
+            res.send(result.acknowledged);
+          });
+        });
+
+        app.get("/getServices", async (req, res) => {
+          try {
+            const documents = await serviceCollection.find().toArray();
+            res.status(200).send(documents);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
+        });
+
+        app.get("/appointments", async (req, res) => {
+          try {
+            const documents = await appointmentCollection.find().toArray();
+            res.status(200).send(documents);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
+        });
+
+        app.get("/checkAdmin", async (req, res) => {
+          try {
+            const documents = await adminCollection
+              .find({ adminEmail: req.query.email })
+              .toArray();
+            res.status(200).send(documents);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
+        });
+
+        app.delete("/deleteService/:id", (req, res) => {
+          serviceCollection
+            .deleteOne({ _id: ObjectID(req.params.id) })
+            .then((result) => {
+              res.send(result.acknowledged);
+            });
+        });
+
+        app.get("/getOrder", async (req, res) => {
+          try {
+            const documents = await orderCollection
+              .find({ adminEmail: req.query.email })
+              .toArray();
+            res.status(200).send(documents);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
+        });
+
+        app.get("/getAppointment", async (req, res) => {
+          try {
+            const documents = await bookingAppointmentCollection
+              .find({ adminEmail: req.query.email })
+              .toArray();
+            res.status(200).send(documents);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
+        });
+
+        app.get("/orderedProduct/:id", async (req, res) => {
+          try {
+            const documents = await productCollection
+              .find({ _id: ObjectID(req.params.id) })
+              .toArray();
+
+            if (documents.length === 0) {
+              try {
+                const appointmentDocuments = await appointmentCollection
+                  .find({ _id: ObjectID(req.params.id) })
+                  .toArray();
+                res.status(200).send(appointmentDocuments[0]);
+              } catch (error) {
+                res.status(500).send("Internal Server Error");
+              }
             } else {
-              res.send(document[0]);
+              res.status(200).send(documents[0]);
             }
-          });
-      });
-      app.get("/allOrders", (req, res) => {
-        orderCollection.find({}).toArray((err, document) => {
-          res.send(document);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
         });
-      });
-      app.get("/bookingAppointment", (req, res) => {
-        bookingAppointmentCollection.find({}).toArray((err, document) => {
-          res.send(document);
+
+        app.get("/allOrders", async (req, res) => {
+          try {
+            const documents = await orderCollection.find({}).toArray();
+            res.status(200).send(documents);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
         });
-      });
-      app.get("/getAllProducts", (req, res) => {
-        productCollection.find({}).toArray((err, document) => {
-          console.log("hi");
-          console.log(err);
-          res.send(document);
+        app.get("/bookingAppointment", async (req, res) => {
+          try {
+            const documents = await bookingAppointmentCollection
+              .find({})
+              .toArray();
+            res.status(200).send(documents);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
         });
-      });
-      app.get("/getTestimonial", (req, res) => {
-        testimonialCollection.find({}).toArray((err, document) => {
-          res.send(document);
+        app.get("/getAllProducts", async (req, res) => {
+          try {
+            const documents = await productCollection.find({}).toArray();
+            res.status(200).send(documents);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
         });
+        app.get("/getTestimonial", async (req, res) => {
+          try {
+            const documents = await testimonialCollection.find({}).toArray();
+            res.status(200).send(documents);
+          } catch (error) {
+            res.status(500).send("Internal Server Error");
+          }
+        });
+        app.patch("/updateStatus/:id", (req, res) => {
+          orderCollection
+            .updateOne(
+              { _id: ObjectID(req.params.id) },
+              {
+                $set: { status: req.body.newStatus },
+              }
+            )
+            .then((result) => {
+              if (result.modifiedCount === 0) {
+                bookingAppointmentCollection.updateOne(
+                  { _id: ObjectID(req.params.id) },
+                  {
+                    $set: { status: req.body.newStatus },
+                  }
+                );
+              } else {
+                res.send(result.modifiedCount > 0);
+              }
+            });
+        });
+      })
+      .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
       });
-      app.patch("/updateStatus/:id", (req, res) => {
-        orderCollection
-          .updateOne(
-            { _id: ObjectID(req.params.id) },
-            {
-              $set: { status: req.body.newStatus },
-            }
-          )
-          .then((result) => {
-            if (result.modifiedCount === 0) {
-              bookingAppointmentCollection.updateOne(
-                { _id: ObjectID(req.params.id) },
-                {
-                  $set: { status: req.body.newStatus },
-                }
-              );
-            } else {
-              res.send(result.modifiedCount > 0);
-            }
-          });
-      });
-    });
 
     // Start Express server
     app.listen(port, () => {
